394370905169d3cba60f0f3744d2dabfb4d79d31
Jonny0430 <ahadjon697gmail.com>
2024-12-26 21:28:13 +0900

    feat: organize and test pㅎㄷㄴ 개ㅕㅅㄷㄱ 뇬ㅅ드

---
+++ pages/property/[productId]/index.tsx
@@ -0,0 +1,11 @@
+import { useRouter } from "next/router";
+
+
+const PropertyDetail = () => {
+    const router = useRouter();
+    const { productId } = router.query;
+    return <div>PROPERTY DETAIL {productId}</div>;
+};
+
+export default PropertyDetail;
+// 37
