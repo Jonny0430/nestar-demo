83689683a5f229aee5c86767cc86530d79ca5acc
Jonny0430 <ahadjon697gmail.com>
2024-12-27 14:59:00 +0900

    feat: Header and Footer via Layouts

---
+++ pages/property/detail.tsx
@@ -0,0 +1,7 @@
+
+import withLayoutBasic from "@/libs/components/layout/LayoutBesic";
+import { Container } from "@mui/material";
+const PropertyDetail = () => {
+  return <Container>PROPERTY DETAIL</Container>;
+};
+export default withLayoutBasic(PropertyDetail);
