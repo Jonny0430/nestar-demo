a883ea38cb94a2ee5c17e07791e1c73a7a151293
Jonny0430 <ahadjon697gmail.com>
2024-12-28 22:01:04 +0900

    feat: SASS integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import { CssBaseline } from "@mui/material";
 import { light } from "@/scss/MaterialTheme";
 import { useState } from "react";
 import { AppProps } from "next/app";
-
+import "../scss/app.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   //@ts-ignore
