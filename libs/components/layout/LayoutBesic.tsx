5a4e6d0ab201d35ccd80d9c1fb7d5d141b39e074
Jonny0430 <ahadjon697gmail.com>
2024-12-29 22:46:17 +0900

    feat: publish basic layout components

--- libs/components/layout/LayoutBesic.tsx
+++ libs/components/layout/LayoutBesic.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/display-name */
 import Head from "next/head"
 import { Stack } from "@mui/material";
+import Top from "../Top";
+import Footer from "../Footer";
 
 
 const withLayoutBasic = (Component: any) => {
@@ -10,11 +12,32 @@ const withLayoutBasic = (Component: any) => {
             <title>Nestar</title>
         </Head>
         <Stack id="pc-wrap">
-    <Stack sx={{ background: "#81c784" }}>Header Besit</Stack>
-        <Stack id={"main"}>
-            <Component {...props} />
-        </Stack>
-    <Stack sx={{ background: "#a1887f" }}>Footer</Stack>
+            <Stack id={"top"}>
+                <Top />
+            </Stack>
+
+            <Stack 
+            className={`header-basic`}
+            style={{
+                backgroundImage: `url(/img/banner/properties.png)`,
+                backgroundSize: "cover",
+                boxShadow:"inset 10px 40px 150px 40px rgb(24 22 36)",
+            }}
+            >
+                <Stack
+                className={"container"}>
+                    <strong>Property Search</strong>
+                    <span>We are glad to see you again!</span>
+                </Stack>
+            </Stack>
+
+            <Stack id={"main"}>
+                <Component {...props} />
+            </Stack>
+
+            <Stack id={"footer"}>
+                <Footer />
+            </Stack>
         </Stack>
         </>
         );
