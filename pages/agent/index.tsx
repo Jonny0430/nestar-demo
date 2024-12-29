5c5d2f96351e099475809918058ff65ab829b3ee
Jonny0430 <ahadjon697gmail.com>
2024-12-29 23:30:38 +0900

    fix: refactor pages commponents

--- pages/agent/index.tsx
+++ pages/agent/index.tsx
@@ -1,12 +1,12 @@
 import withLayoutBasic from "@/libs/components/layout/LayoutBesic";
 import { Stack } from "@mui/material";
 import { NextPage } from "next";
-const PropertyList: NextPage = () => {
+const AgentList: NextPage = () => {
   return (
      <div style={{ margin: "20px 0" }}>
-      <Stack className="container">PROPERTY LIST</Stack>
+      <Stack className="container">AGENT LIST</Stack>
      </div>
   )
 };
 
-export default withLayoutBasic(PropertyList);
+export default withLayoutBasic(AgentList);
